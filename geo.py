3e2c8622b977b04b01b395b5993407882b983c41
haalven <147269352+haalven@users.noreply.github.com>
2025-12-29 17:08:37 +0100

    Refactor geo.py for improved readability and functionality
    Refactor geo.py to simplify API request and response handling, remove unused functions, and improve code readability.

--- geo.py
+++ geo.py
@@ -1,112 +1,44 @@
 #!/usr/bin/env python3
 
 # geo location using ip-api.com
+# next level
 
+import sys, argparse, pathlib, urllib.request, socket, json
 
-import urllib.request
-from json import loads
-from sys import argv, exit
-from socket import gethostbyaddr
-from re  import search
+def main() -> int:
+    # my path
+    my_path = pathlib.Path(__file__)
+    my_name = my_path.name
 
+    # get address argument
+    parser = argparse.ArgumentParser(prog=my_name)
+    parser.add_argument('address', type=str, help='hostname or IP')
+    target = parser.parse_args().address
 
+    # request & response
+    url = 'http://ip-api.com/json/' + target
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        payload = response.read().decode('utf-8')
 
-# api-get
+    # JSON deserialization
+    location = json.loads(payload)
+    if location['status'] == 'fail':
+        print('geo location search failed for', target)
+        return 1
 
-def api_get(url, p):
+    # sorted list
+    sorted_keys = ('country', 'regionName', 'city', 'isp', 'org', 'query')
+    sorted_list = []
+    for key in sorted_keys:
+        sorted_list.append(location[key])
 
-    url = url.strip('/')
-    url += '/' + p
+    # add reverse DNS
+    sorted_list.append(socket.gethostbyaddr(location['query'])[0])
 
-    try:
-        with urllib.request.urlopen(url) as r:
-            return loads(r.read().decode('utf-8'))
-    except Exception as e: print('api error:', e)
+    # output
+    print(' \u203A '.join(sorted_list))
+    return 0
 
-
-
-# addr -> hostname
-
-def hn_by_addr(ipaddr):
-
-    ip = str(ipaddr).strip().lower()
-
-    if not ip: return ''
-
-    try: hostname = str(gethostbyaddr(ip)[0])
-    except Exception: hostname = ''
-
-    return hostname if hostname else '<no hostname>'
-
-
-
-# add info
-
-def add_info(field):
-
-    if field in result:
-
-        if result[field] and not (result[field] in info):
-
-            info.append(result[field].replace(chr(39),chr(700)))
-
-
-
-# api location
-
-url = 'http://ip-api.com/json/'
-
-
-# cli arg
-
-myname = argv.pop(0).split('/')[-1]
-
-syntax = f'syntax: {myname} <address>'
-
-if len(argv) < 1: exit(syntax)
-
-
-# globals
-
-result, info = {}, []
-
-
-# contact api
-
-result = api_get(url, argv[0])
-
-
-# check result
-
-try:
-    if not (result['status'] == 'success'):
-        exit()
-
-except Exception: exit('no result')
-
-
-# fill location info
-
-for f in [  'country',
-            'regionName',
-            'city',
-            'isp',
-            'org',
-            'query' ]:
-
-    add_info(f)
-
-
-# reverse ip -> hostname
-
-if search(r'\d+\.\d+\.\d+\.\d+', result['query']):  # ip4
-
-    hostname = hn_by_addr( result['query'] )
-
-    if hostname: info.append(hostname)
-
-
-# output
-
-if info: print( ' ➔ '.join(info) )
-
+if __name__ == '__main__':
+    sys.exit(main())
